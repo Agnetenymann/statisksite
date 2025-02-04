62c507b3712fa47c7b84872737d477cd5aa2d28d
Agnete Nymann <ag.nymann@gmail.com>
2025-02-04 15:00:38 +0100

    add list

--- productlist.js
+++ productlist.js
@@ -11,15 +11,18 @@ function showProductList(data) {
   const markup = data
     .map(
       (product) => `
-            <article class="product-normal product">
-          <a href="product.html">
-            <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="sort rygsæk" />
+            <article class="product_card">
+                 <a href="product.html?productId=${product.id}">
+                 <div class="container">
+              <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="blå t-shirt" />
+              <div class="sold_out ${product.soldout && "sold_out_true"}">SOLD OUT</div>
+            </div>
             <h3>${product.productdisplayname}</h3>
             <p class="item_brand">${product.articletype} - ${product.brandname}</p>
-            <p class="price"><span>Prev.</span> DKK ${product.price}.-</p>
-            <div class="discount">
-              <p>Now DKK 857.-</p>
-              <p>-34%</p>
+            <p class="price"> <span class="on_sale_prev ${product.discount && "on_sale_prev_true"}" >Prev.</span> DKK ${product.price}.-</p>
+            <div class="on_sale ${product.discount && "is_on_sale"} ">
+            <p> <span class="price_now"> Now </span> ${product.price}</p>
+              <p>-${product.discount}%</p>
             </div>
             <a href="product.html">Shop Now</a>
           </a>
--- produkt.js
+++ produkt.js
@@ -12,7 +12,11 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
         <div class="purchase_box">
           <h4> ${data.productdisplayname} </h4>
           <p class="item_brand">${data.articletype} - ${data.brandname}</p>
-          <p class="price">DKK ${data.price}.-</p>
+          <p class="price"><span>Prev.</span>DKK ${data.price}.-</p>
+            <div class="on_sale_label ${data.discount && "is_on_sale_label"}">
+              <p>-${data.discount}%</p>
+            </div>
+
           <form>
             <label>
               Choose a size:
@@ -41,3 +45,47 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
         </div>
     `;
   });
+
+// let productId = 1526;
+// let productOverview = document.querySelector(".product_overview");
+
+// fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     productOverview.innerHTML = `
+//     <div>
+//           <img src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp" alt="puma rygsæk" />
+//         </div>
+//         <div class="purchase_box">
+//           <h4> ${data.productdisplayname} </h4>
+//           <p class="item_brand">${data.articletype} - ${data.brandname}</p>
+//           <p class="price">DKK ${data.price}.-</p>
+//           <form>
+//             <label>
+//               Choose a size:
+//               <select name="size">
+//                 <option value="OS">Onesize</option>
+
+//               </select>
+//             </label>
+//             <div><button>Add to basket</button></div>
+//           </form>
+
+//           <section class="product_info">
+//             <h2>Product information</h2>
+//             <dl>
+//               <dt>Model name</dt>
+//               <dd>${data.productdisplayname}</dd>
+//               <dt>Season</dt>
+//               <dd>${data.season}</dd>
+//               <dt>Category</dt>
+//               <dd>${data.articletype}</dd>
+//               <dt>Brand</dt>
+//               <dd>${data.brandname}</dd>
+//               <dt>Intentory number</dt>
+//               <dd>${data.id}</dd>
+//             </dl>
+//           </section>
+//         </div>
+//     `;
+//   });
